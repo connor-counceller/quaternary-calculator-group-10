c1d86e0a7cdc353e41df06d31608075c700d27aa
Kris Thurman <kristhurman12@gmail.com>
2023-09-07 13:11:47 -0400

    add calculator layout, includes numbers 0 - 3 and mathematical operators that don't work

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,40 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
 public class Calculator {
-    public static void main(String[] args) {
+    private JFrame frame;
+    private JTextField display;
+    public Calculator() {
+        frame = new JFrame("Quaternary Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);
 
+        display = new JTextField(10);
+        display.setFont(new Font("Arial", Font.PLAIN, 24));
+        display.setEditable(false);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(5, 4));
+        String[] buttonLabels = {
+          "0", "1", "+", "2",
+                "3", "-", "*", "/",
+                "=", "<--", "CLR", " "
+        };
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().add(display, BorderLayout.NORTH);
+        frame.getContentPane().add(buttonPanel, BorderLayout.CENTER);
+
+        frame.setVisible(true);
     }
-
     public int example(int numOne, int numTwo) {
         return numOne+numTwo;
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Calculator();
+            }
+        });
     }
 }
--- tests/CalculatorTest.java
+++ tests/CalculatorTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-
     @Test
     void example() {
         Calculator calculator = new Calculator();
