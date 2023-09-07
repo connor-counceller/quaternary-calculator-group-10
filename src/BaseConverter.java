17a6a37e5211d70cf15cfc700fdf2263daad3970
Connor Counceller <cc.novae@gmail.com>
2023-09-07 19:05:21 -0400

    Add basic functionality to BaseConverter.baseFourToBaseTen

--- src/BaseConverter.java
+++ src/BaseConverter.java
@@ -1,7 +1,16 @@
 public class BaseConverter {
 
     public int baseFourToBaseTen(int baseFourInt) {
-        return 0;
+        String baseFourString = Integer.toString(baseFourInt);
+        int baseFourDigits = baseFourString.length();
+        int baseTenInt = 0;
+        int currentDigit;
+
+        for (int i = 0; i < baseFourDigits; i++) {
+            currentDigit = Character.getNumericValue(baseFourString.charAt(i));
+            baseTenInt += currentDigit * (Math.pow(4, (baseFourDigits - i - 1)));
+        }
+        return baseTenInt;
     }
 
     public int baseTenToBaseFour(int baseTenInt) {
--- tests/BaseConverterTest.java
+++ tests/BaseConverterTest.java
@@ -5,14 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class BaseConverterTest {
 
     @Test
+    void baseFourToBaseTen() {
+        BaseConverter baseConverter = new BaseConverter();
+        assertEquals(1, baseConverter.baseFourToBaseTen(1));
+    }
+
+    @Test
     void baseTenToBaseFour() {
         BaseConverter baseConverter = new BaseConverter();
         assertEquals(0, baseConverter.baseTenToBaseFour(0));
     }
 
-    @Test
-    void baseFourToBaseTen() {
-        BaseConverter baseConverter = new BaseConverter();
-        assertEquals(0, baseConverter.baseFourToBaseTen(0));
-    }
+
 }
