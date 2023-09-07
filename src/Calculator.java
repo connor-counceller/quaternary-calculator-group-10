23a7c397a8e09481d6b54e9aa4b4fb3879c5dd59
Kris Thurman <kristhurman12@gmail.com>
2023-09-07 13:19:24 -0400

    add calculator layout, includes numbers 0 - 3 and mathematical operators that don't work

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculator {
     private JFrame frame;
     private JTextField display;
+    private String currentInput;
+
+    private boolean isOperatorClicked;
+
+
     public Calculator() {
         frame = new JFrame("Quaternary Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,11 +28,31 @@ public class Calculator {
                 "3", "-", "*", "/",
                 "=", "<--", "CLR", " "
         };
+        for (String label : buttonLabels) {
+            JButton button = new JButton(label);
+            button.addActionListener(new buttonClickListener());
+            button.setFont(new Font("Arial", Font.PLAIN, 18));
+            buttonPanel.add(button);
+        }
         frame.getContentPane().setLayout(new BorderLayout());
         frame.getContentPane().add(display, BorderLayout.NORTH);
         frame.getContentPane().add(buttonPanel, BorderLayout.CENTER);
 
         frame.setVisible(true);
+    }
+    private class buttonClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+            if (command.matches("[0-3]")) {
+                if (isOperatorClicked) {
+                    currentInput = "";
+                    isOperatorClicked = false;
+                }
+                currentInput += command;
+                display.setText(currentInput);
+            }
+        }
+
     }
     public int example(int numOne, int numTwo) {
         return numOne+numTwo;
