56f1e450d6b00983d5359e3ef8aa6ab582a1f0e4
Connor Counceller <cc.novae@gmail.com>
2023-09-07 19:10:46 -0400

    Add test cases for BaseConverter.baseFourToBaseTen

--- tests/BaseConverterTest.java
+++ tests/BaseConverterTest.java
@@ -8,6 +8,10 @@ class BaseConverterTest {
     void baseFourToBaseTen() {
         BaseConverter baseConverter = new BaseConverter();
         assertEquals(1, baseConverter.baseFourToBaseTen(1));
+        assertEquals(6, baseConverter.baseFourToBaseTen(12));
+        assertEquals(27, baseConverter.baseFourToBaseTen(123));
+        assertEquals(108, baseConverter.baseFourToBaseTen(1230));
+        assertEquals(433, baseConverter.baseFourToBaseTen(12301));
     }
 
     @Test
