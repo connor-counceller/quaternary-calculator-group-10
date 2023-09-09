541cafb0b8bae9a9b7b507ec581fd2ed61d461ee
cjbecker21 <77917956+cjbecker21@users.noreply.github.com>
2023-09-09 09:58:40 -0500

    Added operations to buttons and logic. Used Array for multiple inputs. Created Toggle function .

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,14 +3,19 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-// produces null in calculator
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
     private JFrame frame;
     private JTextField display;
     private String currentInput;
-
-    private boolean isOperatorClicked;
-
+    private List<Integer> baseFourNumbers;
+    private int baseTenResult;
+    private int baseFourResult;
+    private String operator;
+    private boolean isDisplayingBaseTen;
+    private BaseConverter baseConverter; // Added BaseConverter instance
 
     public Calculator() {
         frame = new JFrame("Quaternary Calculator");
@@ -24,13 +29,13 @@ public class Calculator {
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(5, 4));
         String[] buttonLabels = {
-          "0", "1", "+", "2",
+                "0", "1", "+", "2",
                 "3", "-", "*", "/",
                 "=", "<--", "CLR", " "
         };
         for (String label : buttonLabels) {
             JButton button = new JButton(label);
-            button.addActionListener(new buttonClickListener());
+            button.addActionListener(new ButtonClickListener());
             button.setFont(new Font("Arial", Font.PLAIN, 18));
             buttonPanel.add(button);
         }
@@ -38,25 +43,84 @@ public class Calculator {
         frame.getContentPane().add(display, BorderLayout.NORTH);
         frame.getContentPane().add(buttonPanel, BorderLayout.CENTER);
 
+        baseFourNumbers = new ArrayList<>();
+        currentInput = "";
+        baseTenResult = 0;
+        baseFourResult = 0;
+        operator = "";
+        isDisplayingBaseTen = true;
+        baseConverter = new BaseConverter(); // Initialize BaseConverter
+
         frame.setVisible(true);
     }
-    private class buttonClickListener implements ActionListener {
+
+    private class ButtonClickListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
             if (command.matches("[0-3]")) {
-                if (isOperatorClicked) {
-                    currentInput = "";
-                    isOperatorClicked = false;
-                }
                 currentInput += command;
                 display.setText(currentInput);
+            } else if (command.equals("+") || command.equals("-") || command.equals("*") || command.equals("/")) {
+                if (!currentInput.isEmpty()) {
+                    baseFourNumbers.add(Integer.parseInt(currentInput));
+                    currentInput = "";
+                }
+                operator = command;
+            } else if (command.equals("=")) {
+                if (!currentInput.isEmpty()) {
+                    baseFourNumbers.add(Integer.parseInt(currentInput));
+                    calculateResult();
+                }
+            } else if (command.equals("CLR")) {
+                currentInput = "";
+                baseFourNumbers.clear();
+                baseTenResult = 0;
+                baseFourResult = 0;
+                operator = "";
+                display.setText("");
+            } else if (command.equals("<--")) {
+                isDisplayingBaseTen = !isDisplayingBaseTen;
+                updateDisplay();
             }
         }
 
+        private void calculateResult() {
+            if (!baseFourNumbers.isEmpty()) {
+                baseTenResult = baseFourNumbers.get(0);
+                for (int i = 1; i < baseFourNumbers.size(); i++) {
+                    int baseTenOperand = baseFourNumbers.get(i);
+                    if (operator.equals("+")) {
+                        baseTenResult += baseTenOperand;
+                    } else if (operator.equals("-")) {
+                        baseTenResult -= baseTenOperand;
+                    } else if (operator.equals("*")) {
+                        baseTenResult *= baseTenOperand;
+                    } else if (operator.equals("/")) {
+                        if (baseTenOperand != 0) {
+                            baseTenResult /= baseTenOperand;
+                        } else {
+                            // Handle division by zero error
+                            display.setText("Error: Division by zero");
+                            return;
+                        }
+                    }
+                }
+                baseFourResult = baseConverter.baseTenToBaseFour(baseTenResult);
+                updateDisplay();
+                baseFourNumbers.clear();
+                currentInput = Integer.toString(baseFourResult);
+            }
+        }
+
+        private void updateDisplay() {
+            if (isDisplayingBaseTen) {
+                display.setText("Base 10: " + baseTenResult);
+            } else {
+                display.setText("Base 4: " + baseFourResult);
+            }
+        }
     }
-    public int example(int numOne, int numTwo) {
-        return numOne+numTwo;
-    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
@@ -65,3 +129,4 @@ public class Calculator {
         });
     }
 }
+
