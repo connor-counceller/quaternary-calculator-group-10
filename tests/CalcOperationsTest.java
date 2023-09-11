d62fcfec1a16f5973e100162c76fdb10a16f2aa0
Micah Harker <micah.harker@gmail.com>
2023-09-10 22:15:47 -0400

    Create tests for CalcOperations

--- tests/CalcOperationsTest.java
+++ tests/CalcOperationsTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalcOperationsTest {
+
+    @Test
+
+    void getResult(){
+        CalcOperations calcOperations = new CalcOperations();
+        assertEquals(1, calcOperations.getResult("+", 0, 1));
+        assertEquals(1, calcOperations.getResult("-", 5, 4));
+        assertEquals(20, calcOperations.getResult("*", 5, 4));
+        assertEquals(2, calcOperations.getResult("/", 10, 5));
+    }
 }
